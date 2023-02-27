ec7ad84f1b3dd34939133e59468f08f9774e8e19
Rosario Moscato <rosariomoscatolab@gmail.com>
2023-02-27 11:22:19 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+import PDFLib
+
+PDFLib.set_debugMode(0)
+
+#metadata = PDFLib.get_metadata("Listino_Sviluppi_Stampe.pdf")
+#print(metadata)
+
+#help(PDFLib.get_metadata)
+#help(PDFLib.extract_text)
+
+#text = PDFLib.extract_text("Listino_Sviluppi_Stampe.pdf")
+#print(text)
+
+#help(PDFLib.pages_splitter)
+#PDFLib.pages_splitter("Listino_Sviluppi_Stampe.pdf")
+
+#help(PDFLib.fromto_splitter)
+#PDFLib.fromto_splitter("MG719_ZS_Brochure_WEB_ITA.pdf",1)
+
+#help(PDFLib.last_page)
+#PDFLib.last_page("MG719_ZS_Brochure_WEB_ITA.pdf")
+
+#help(PDFLib.pdf_files_collector)
+#files = PDFLib.pdf_files_collector("./samples")
+#print(files)
+
+#help(PDFLib.pdf_list_merger)
+#files_list = PDFLib.pdf_files_collector("./samples")
+#PDFLib.pdf_list_merger(files_list)
+
+#help(PDFLib.pdf_folder_merger)
+#PDFLib.pdf_folder_merger("./samples")
+
+#help(PDFLib.pdf_rotate)
+#PDFLib.pdf_rotate("Listino_Sviluppi_Stampe.pdf",1)
+
+#help(PDFLib.pdf_images_extractor)
+#PDFLib.pdf_images_extractor("MG719_ZS_Brochure_WEB_ITA_from_1_to_3.pdf")
+
+#help(PDFLib.img2pdf)
+#PDFLib.img2pdf("Im0.jpg")
+
+PDFLib.info()
